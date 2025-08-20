31f5ed7d489144f5ec5fdd298c8e22e83898ef34
SOMMHEO <hsm8506@naver.com>
2025-08-20 18:23:08 +0900

    21th

--- src/labeling_main.py
+++ src/labeling_main.py
@@ -65,17 +65,17 @@ def main():
     # new_media_data = pd.concat(media_dfs, ignore_index=True)
 
     profile_prefixes = [
-        f'instagram-data/tables/EXTERNAL_RECENT_USER_INFO_MTR/year={year}/week=33',
-        f'instagram-data/tables/EXTERNAL_2_RECENT_USER_INFO_MTR/year={year}/week=30',
-        f'instagram-data/tables/RECENT_USER_INFO_MTR/year={year}/week={week}/'
-        # f'instagram-data/tables/CONN_v2_RECENT_USER_INFO_MTR/2025-08-13/'
+        # f'instagram-data/tables/EXTERNAL_RECENT_USER_INFO_MTR/year={year}/week=33',
+        # f'instagram-data/tables/EXTERNAL_2_RECENT_USER_INFO_MTR/year={year}/week=30',
+        # f'instagram-data/tables/RECENT_USER_INFO_MTR/year={year}/week={week}/'
+        f'instagram-data/tables/CONN_v2_RECENT_USER_INFO_MTR/2025-08-14/'
     ]
 
     media_prefixes = [
-        f'instagram-data/tables/EXTERNAL_BY_USER_ID_MEDIA_DTL_INFO/year={year}/week=33',
-        f'instagram-data/tables/EXTERNAL_2_BY_USER_ID_MEDIA_DTL_INFO/year={year}/week=30',
-        f'instagram-data/tables/BY_USER_ID_MEDIA_DTL_INFO/year={year}/week={week}/'
-        # f'instagram-data/tables/CONN_v2_BY_USER_ID_MEDIA_DTL_INFO/2025-08-13/'
+        # f'instagram-data/tables/EXTERNAL_BY_USER_ID_MEDIA_DTL_INFO/year={year}/week=33',
+        # f'instagram-data/tables/EXTERNAL_2_BY_USER_ID_MEDIA_DTL_INFO/year={year}/week=30',
+        # f'instagram-data/tables/BY_USER_ID_MEDIA_DTL_INFO/year={year}/week={week}/'
+        f'instagram-data/tables/CONN_v2_BY_USER_ID_MEDIA_DTL_INFO/2025-08-14/'
     ]
 
     # 모든 파일을 저장할 리스트
@@ -124,36 +124,37 @@ def main():
         print("\nNo media data found.")
 
 
-    # # category labeling
-    # category_labels = ['IT', '게임', '결혼/연애', '교육', '다이어트/건강보조식품', '만화/애니/툰', '문구/완구', '미술/디자인', '반려동물', '베이비/키즈', '뷰티', '브랜드공식계정',
-    #                 '사진/영상', '셀럽', '스포츠', '시사', '엔터테인먼트', '여행/관광', '유명장소/핫플', '일상', '자동차/모빌리티', '짤/밈', '취미', '패션', '푸드', '홈/리빙']
+    # category labeling
+    category_labels = ['IT', '게임', '결혼/연애', '교육', '다이어트/건강보조식품', '만화/애니/툰', '문구/완구', '미술/디자인', '반려동물', '베이비/키즈', '뷰티', '브랜드공식계정',
+                    '사진/영상', '셀럽', '스포츠', '시사', '엔터테인먼트', '여행/관광', '유명장소/핫플', '일상', '자동차/모빌리티', '짤/밈', '취미', '패션', '푸드', '홈/리빙']
 
-    # merged_df, predict_df = tokenize_and_predict_batch(new_profile_data, new_media_data, category_labels)
-    # merged_df = merged_df[['acnt_id', 'acnt_nm']].reset_index(drop=True)
-    # predict_df.reset_index(drop=True, inplace=True)
+    merged_df, predict_df = tokenize_and_predict_batch(new_profile_data, new_media_data, category_labels)
+    merged_df = merged_df[['acnt_id', 'acnt_nm']].reset_index(drop=True)
+    predict_df.reset_index(drop=True, inplace=True)
     
-    # # final data after category labeling
-    # final_predict_df = pd.concat([merged_df, predict_df], axis=1)
+    # final data after category labeling
+    final_predict_df = pd.concat([merged_df, predict_df], axis=1)
     # final_predict_df.to_csv("flexmatch_influencer_category_matching.csv")  # 확인
     
-    # ## merge data with DB data - only flexmatch influencer
-    # # preprocessing DB data
-    # flexmatch_influencer_info = get_all_infos()
-    # flexmatch_influencer_info['member_uid'] = flexmatch_influencer_info['member_uid'].fillna(0).astype(int)
-    # flexmatch_influencer_info['add1'] = flexmatch_influencer_info['add1'].str.replace('https://www.instagram.com/', '')
-    # flexmatch_influencer_info['acnt_nm'] = flexmatch_influencer_info['add1'].str.replace('/', '')
+    ## merge data with DB data - only flexmatch influencer
+    # preprocessing DB data
+    flexmatch_influencer_info = get_all_infos()
+    flexmatch_influencer_info['member_uid'] = flexmatch_influencer_info['member_uid'].fillna(0).astype(int)
+    flexmatch_influencer_info['add1'] = flexmatch_influencer_info['add1'].str.replace('https://www.instagram.com/', '')
+    flexmatch_influencer_info['acnt_nm'] = flexmatch_influencer_info['add1'].str.replace('/', '')
 
-    # db_merge_df = pd.merge(final_predict_df, flexmatch_influencer_info, on='acnt_nm', how='left')
+    db_merge_df = pd.merge(final_predict_df, flexmatch_influencer_info, on='acnt_nm', how='left')
 
-    # main_category = db_merge_df.groupby(['acnt_id', 'acnt_nm'])['bert_top_label'].agg(lambda x: x.value_counts().idxmax()).to_frame().reset_index().rename(columns={'bert_top_label' : 'main_category'})
+    main_category = db_merge_df.groupby(['acnt_id', 'acnt_nm'])['bert_top_label'].agg(lambda x: x.value_counts().idxmax()).to_frame().reset_index().rename(columns={'bert_top_label' : 'main_category'})
 
-    # top_3_labels = db_merge_df.groupby(['acnt_id', 'acnt_nm'])['bert_top_label'].value_counts().groupby(level=[0,1]).head(3).reset_index(name='count')
-    # top_3_labels_joined = (top_3_labels.groupby(['acnt_id', 'acnt_nm'])['bert_top_label'].apply(lambda x: '@'.join(x)).reset_index(name='top_3_category'))
+    top_3_labels = db_merge_df.groupby(['acnt_id', 'acnt_nm'])['bert_top_label'].value_counts().groupby(level=[0,1]).head(3).reset_index(name='count')
+    top_3_labels_joined = (top_3_labels.groupby(['acnt_id', 'acnt_nm'])['bert_top_label'].apply(lambda x: '@'.join(x)).reset_index(name='top_3_category'))
     
-    # top_3_labels_joined = top_3_labels_joined.drop(columns=['acnt_id', 'acnt_nm'])
-    # final_df = pd.concat([main_category, top_3_labels_joined], axis=1)
+    top_3_labels_joined = top_3_labels_joined.drop(columns=['acnt_id', 'acnt_nm'])
+    final_df = pd.concat([main_category, top_3_labels_joined], axis=1)
+    print(final_df)
 
-    # # DB Insert
+    # DB Insert
     # data_list = final_df.to_dict(orient='records')
     
     # ssh = SSHMySQLConnector()
